272f34c64ffc964b93510a8fda6a74b0569fcfff
Marqis Ramos <atair01@yahoo.com>
2019-06-21 15:04:11 -0700

    not feeling too great today, tried to sleep it off but didnt work, will finish and submit project saturday.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,38 @@
 $(document).ready(function(){
+
+  var order = new Order();
+  var size;
+  var toppings;
+
+  $(function(){
+    function Order () {
+      this.order = [],
+      this.currentId= 0
+    }
+
+    Order.prototype.addOrder = function (newOrder) {
+      newOrder.id = this.assignId();
+      this.order.push(neworder);
+    }
+    Order.prototype.assignId = function () {
+      this.currentId +=1;
+      return this.currentId;
+    }
+
+    function Size (name, balance) {
+      this.name = name,
+      this.size = size
+    }
+
+    Account.prototype.Size = function(size) {
+      this.size += size;
+      this.size.push(size);
+      return this.size;
+    }
+    Account.prototype.Toppings = function(toppings) {
+      this.toppings.push(toppings);toppings
+      this.balance -= toppings;
+      return this.size;
+    }
+    });
+  });
