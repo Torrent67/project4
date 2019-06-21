f88a99941e41e8f5e30df4e2aeb11acad41e121d
Marqis Ramos <atair01@yahoo.com>
2019-06-21 16:24:45 -0700

    trying to finish might be a late submit, will show pizza optiions or sizes and toppings

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,12 @@ $(document).ready(function(){
   var size;
   var toppings;
 
-  $(function(){
-    function Order () {
-      this.order = [],
-      this.currentId= 0
-    }
 
     Order.prototype.addOrder = function (newOrder) {
       newOrder.id = this.assignId();
       this.order.push(neworder);
     }
-    Order.prototype.assignId = function () {
-      this.currentId +=1;
-      return this.currentId;
-    }
 
-    function Size (name, balance) {
-      this.name = name,
-      this.size = size
-    }
 
     Account.prototype.Size = function(size) {
       this.size += size;
@@ -35,4 +22,9 @@ $(document).ready(function(){
       return this.size;
     }
     });
+    function Order () {
+      this.order = [];
+      this.currentId= 0
+    }
+
   });
