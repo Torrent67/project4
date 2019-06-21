09d35a7ec7dee5d067bf3f1e0b593dc53ecebcda
Marqis Ramos <atair01@yahoo.com>
2019-06-21 16:53:13 -0700

    webpage will look cleaner, working on backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,26 @@
 $(document).ready(function(){
 
   var order = new Order();
-  var size;
-  var toppings;
+  var size
+  var toppings = [];
+
+  function Order() {
+    this.order = [];
+    this.currentId= 0
+}
+
+  Order.prototype.addOrder = function (name, time, size, toppings) {
+  this.name = name;
+  this.time = time;
+  this.size = size;
+  order.addToppings ();
+  this.order.push(neworder);
+}
+
+  Order.prototype.addToppings = function(toppings) {
+  this.toppings.push(toppings);
+}
 
 
-    Order.prototype.addOrder = function (newOrder) {
-      newOrder.id = this.assignId();
-      this.order.push(neworder);
-    }
 
-
-    Account.prototype.Size = function(size) {
-      this.size += size;
-      this.size.push(size);
-      return this.size;
-    }
-    Account.prototype.Toppings = function(toppings) {
-      this.toppings.push(toppings);toppings
-      this.balance -= toppings;
-      return this.size;
-    }
-    });
-    function Order () {
-      this.order = [];
-      this.currentId= 0
-    }
-
-  });
+});
